bb2a97db62dcf4e88eeb81ef848e88297e287e99
Clésio Gonçalves <clesio.pi@hotmail.com>
2018-07-28 19:09:26 -0300

    Login com o Spring Security

---
+++ WebContent/WEB-INF/views/erro/403.jsp
@@ -0,0 +1,15 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>403</title>
+<c:import url="../componentes/cabecalho.jsp" />
+<div class="jumbotron">
+	<div class="container">
+		<h1>Acesso negado.</h1>
+	</div>
+</div>
+
+<c:import url="../componentes/rodape.jsp" />
---
+++ src/print/capau/springsecurity/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package print.capau.springsecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import print.capau.dao.UsuarioDao;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UsuarioDao userDetailsService;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.
+			authorizeRequests()
+			.anyRequest()
+			.authenticated()
+		.and()
+		.formLogin()
+			.loginPage("/login")
+			.permitAll()
+		.and()
+			.logout()
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.permitAll();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+		//auth.inMemoryAuthentication().withUser("clesio").password("{noop}clesio").roles("ADMIN");
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**");
+	}
+
+}
